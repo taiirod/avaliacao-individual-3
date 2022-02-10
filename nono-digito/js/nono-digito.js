f0374e74ea6d9732e92bd55e155115c805f33db2
Tainan Rodrigues <tainansilvasci@gmail.com>
2022-02-10 18:20:12 -0300

    ajustes para finalização de exercício 2

--- nono-digito/js/nono-digito.js
+++ nono-digito/js/nono-digito.js
@@ -1,53 +1,56 @@
 const campoCpf = document.querySelector("#campo-cpf");
 
-campoCpf.addEventListener("input", pegaNonoDigito);
+campoCpf.addEventListener("input", validaRegiao);
 
-function pegaNonoDigito() {
+function validaRegiao() {
     var cpf = campoCpf.value;
-    if (cpf.length === 11) {
-        var nonoDigito = cpf.substr(8, 1);
-        validaRegiao(nonoDigito);
+    if (cpf.length === 11 && validaCpf(cpf)) {
+        var nonoDigito = pegaNonoDigito(cpf)
+        cpf = formataCpf(cpf)
+        var descricaoRegiao = "";
+        switch (nonoDigito) {
+            case "1":
+                descricaoRegiao = `A região do cpf ${cpf} é: 1ª: DF, GO, MT, MS e TO`;
+                break;
+            case "2":
+                descricaoRegiao = `A região do cpf ${cpf} é: 2ª: AC, AP, AM, PA, RO e RR`;
+                break;
+            case "3":
+                descricaoRegiao = `A região do cpf ${cpf} é: 3ª: CE, MA e PI`;
+                break;
+            case "4":
+                descricaoRegiao = `A região do cpf ${cpf} é: 4ª: AL, PB, PE e RN`;
+                break;
+            case "5":
+                descricaoRegiao = `A região do cpf ${cpf} é: 5ª: BA e SE`;
+                break;
+            case "6":
+                descricaoRegiao = `A região do cpf ${cpf} é: 6ª: MG`;
+                break;
+            case "7":
+                descricaoRegiao = `A região do cpf ${cpf} é: 7ª: ES e RJ`;
+                break;
+            case "8":
+                descricaoRegiao = `A região do cpf ${cpf} é: 8ª: SP`;
+                break;
+            case "9":
+                descricaoRegiao = `A região do cpf ${cpf} é: 9ª: PR e SC`;
+                break;
+            case "0":
+                descricaoRegiao = `A região do cpf ${cpf} é: 10.ª: RS`;
+                break;
+            default:
+                descricaoRegiao = "Região inválida";
+                break;
+        }
+        descreveRegiao(descricaoRegiao);
+    } else if (cpf.length === 11 && !validaCpf(cpf)){
+        descreveRegiao("CPF Inválido");
     }
 }
 
-function validaRegiao(nonoDigito) {
-    var descricaoRegiao = "";
-    switch (nonoDigito) {
-        case "1":
-            descricaoRegiao = "Sua região é: 1ª: DF, GO, MT, MS e TO";
-            break;
-        case "2":
-            descricaoRegiao = "Sua região é: 2ª: AC, AP, AM, PA, RO e RR";
-            break;
-        case "3":
-            descricaoRegiao = "Sua região é: 3ª: CE, MA e PI";
-            break;
-        case "4":
-            descricaoRegiao = "Sua região é: 4ª: AL, PB, PE e RN";
-            break;
-        case "5":
-            descricaoRegiao = "Sua região é: 5ª: BA e SE";
-            break;
-        case "6":
-            descricaoRegiao = "Sua região é: 6ª: MG";
-            break;
-        case "7":
-            descricaoRegiao = "Sua região é: 7ª: ES e RJ";
-            break;
-        case "8":
-            descricaoRegiao = "Sua região é: 8ª: SP";
-            break;
-        case "9":
-            descricaoRegiao = "Sua região é: 9ª: PR e SC";
-            break;
-        case "0":
-            descricaoRegiao = "Sua região é: 10.ª: RS";
-            break;
-        default:
-            descricaoRegiao = "Região inválida";
-            break;
-    }
-    descreveRegiao(descricaoRegiao);
+function pegaNonoDigito(cpf) {
+    return cpf.substr(8, 1);
 }
 
 function descreveRegiao(mensagem) {
@@ -57,8 +60,8 @@ function descreveRegiao(mensagem) {
     novaMensagem.appendChild(texto)
     novaMensagem.id = "descricao-regiao"
 
-   novaMensagem.style.marginTop = "30px";
-   novaMensagem.style.fontSize = "30px";
+    novaMensagem.style.marginTop = "30px";
+    novaMensagem.style.fontSize = "30px";
 
     document.querySelector(".row").appendChild(novaMensagem);
 }
@@ -68,4 +71,29 @@ function removeDescricaoAnterior() {
     if (remove != null) {
         remove.remove();
     }
+}
+
+function formataCpf(cpf) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+function validaCpf(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
 }
